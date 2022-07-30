c69ae60cb05c794c52dac68b75456d81114d1b7d
ayrtonlacerda <ayrtonrodrigues.lacerda@gmail.com>
2022-07-30 11:06:42 -0300

    feat: add theme with zustand

--- packages/ui/src/Box.tsx
+++ packages/ui/src/Box.tsx
@@ -7,6 +7,8 @@ import {
   ViewProps,
 } from "react-native";
 
+import { theme } from ".";
+
 const { width: screenWidth } = Dimensions.get("window");
 
 /* 
@@ -57,7 +59,6 @@ const layout = {
 COLORS
 */
 const colors = {
-  color: "color",
   bg: "backgroundColor",
 };
 
@@ -84,6 +85,8 @@ const px = (valuePx: number) => {
 
 // chave é o alias se nao existe e a do stylesheet
 const normalize = (key: string, value: number | string) => {
+  const { sizes } = theme;
+
   const include = [
     Object.keys(padding),
     Object.keys(margin),
@@ -99,11 +102,29 @@ const normalize = (key: string, value: number | string) => {
     "right",
   ];
 
-  if (include.find((i) => i === key) && !isNaN(Number(value)))
-    return px(Number(value));
+  // se tem no objeto size um alias pega o valor do alias se nao passar o valor
+  if (include.find((i) => i === key))
+    return px(Number(typeof value === "string" ? sizes[value] : value));
 
   return value;
 };
+
+export const getColor = (key: string) => {
+  console.log("key ----> ", key);
+  const { colors } = theme;
+  if (!key) return null;
+
+  if (String(key).includes(".")) {
+    const splited = key.split(".");
+    return colors[splited[0]][splited[1]] || "#fff";
+  }
+
+  return (
+    colors[key] ||
+    ((String(key).includes("rgb") || String(key).includes("#")) && key)
+  );
+};
+
 /**
  * Cria o style com stylesheet, pra isso verifica primeiro se a prop é um alias
  * depois normaliza o valor se fizer parte do array que inclui essas props
@@ -111,24 +132,36 @@ const normalize = (key: string, value: number | string) => {
 const createStyle = (props) => {
   let newProps = props;
   Object.keys(newProps).forEach((key) => {
+    const value = newProps[key];
+
+    // lida com o alias background para aplicar cor do theme ou a propria cor
+    if (key === "bg") {
+      Object.defineProperty(newProps, aliasProps[key], getColor(value));
+      delete props[key];
+      return;
+    }
+
+    // lida com o alias que não são de cor
     if (aliasProps[key]) {
       Object.defineProperty(
         newProps,
         aliasProps[key],
-        normalize(
-          aliasProps[key],
-          newProps && Object.getOwnPropertyDescriptor(newProps, key)
-        )
+        normalize(aliasProps[key], value)
       );
       delete props[key];
       return;
     }
-    Object.defineProperty(
-      newProps,
-      key,
-      normalize(key, newProps && Object.getOwnPropertyDescriptor(newProps, key))
-    );
+
+    // lida com as key de cor
+    if (key === "borderColor" || key === "color") {
+      Object.defineProperty(newProps, key, getColor(value));
+      return;
+    }
+
+    // lida com todas as outras keys que nao são de cor ou de alias
+    Object.defineProperty(newProps, key, normalize(key, value));
   });
+
   return StyleSheet.create({ ...newProps, ...props });
 };
 
