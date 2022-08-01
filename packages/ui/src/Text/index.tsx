96181405ded8fa12e769afe2ef5656468c6fc1e2
ayrtonlacerda <ayrtonrodrigues.lacerda@gmail.com>
2022-08-01 12:44:20 -0300

    feat: add text component

--- packages/ui/src/Text/index.tsx
+++ packages/ui/src/Text/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
-import { Text as TextRN } from 'react-native'
-import { useComponentStyle } from '@sevenapps/theme'
+import { Text as TextRN, TextInputProps } from 'react-native'
+import { useComponentStyle, FontTypes, ColorTypes } from '@sevenapps/theme'
+interface TextInterface extends TextInputProps {
+  fontSize: FontTypes | number
+  color: ColorTypes | string
+}
 
-export const Text = ({ children, ...props }) => {
+export const Text = ({ children, ...props }: TextInterface & FontTypes) => {
   const style = useComponentStyle(props, 'Text')
 
   return (
